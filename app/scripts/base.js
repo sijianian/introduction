ae4fdf7c5b44da9de5d74c1c3f934f8f028cc71c
moke <444295467@qq.com>
2017-02-17 18:28:29 +0800

    个人介绍初步完成

---
+++ app/scripts/base.js
@@ -0,0 +1,41 @@
+requirejs.config({
+    baseUrl: 'scripts',
+    paths: {
+        'jquery': 'jquery',
+        'fullpage': 'fullpage',
+        'materialize': 'materialize'
+
+    },
+    shim: {
+        'fullpage': ['jquery'],
+        'materialize': ['jquery']
+    }
+})
+
+requirejs(['jquery', 'fullpage', 'materialize'], function($) {
+    $(function() {
+        var runPage = new FullPage({
+            id: 'pageContain', // id of contain
+            slideTime: 1000, // time of slide
+            continuous: true, // create an infinite feel with no endpoints
+            effect: { // slide effect
+                transform: {
+                    translate: 'Y', // 'X'|'Y'|'XY'|'none'
+                    scale: [.1, 1], // [scalefrom, scaleto]
+                    rotate: [60, 0] // [rotatefrom, rotateto]
+                },
+                opacity: [0, 1] // [opacityfrom, opacityto]
+            },
+            mode: 'wheel,touch,nav:navBar', // mode of fullpage
+            easing: 'ease-in-out' // easing('ease','ease-in','ease-in-out' or use cubic-bezier like [.33, 1.81, 1, 1];
+        });
+        $('.modal').modal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .1, // Opacity of modal background
+            inDuration: 300, // Transition in duration
+            outDuration: 200, // Transition out duration
+            startingTop: '4%', // Starting top style attribute
+            endingTop: '10%', // Ending top style attribute
+        });
+    })
+})
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,7 @@ import sass from 'gulp-sass'
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
+
 /*清除dist文件夹*/
 gulp.task('clean', () => {
     return gulp.src(['dist'])
@@ -26,6 +27,7 @@ gulp.task('styles', () => {
         }).on('error', sass.logError))
         .pipe($.sourcemaps.write('.')) //map 文件名
         .pipe(gulp.dest('dist/styles')) //指定输出路径
+        .pipe(browserSync.stream());
 });
 
 /*转化es6的js*/
@@ -34,11 +36,38 @@ gulp.task('scripts', () => {
         .pipe($.sourcemaps.init())
         .pipe($.babel())
         .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/scripts'));
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(browserSync.stream());
+});
+
+/*html流*/
+gulp.task('html', () => {
+    return gulp.src('app/*.html')
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream());
+});
+
+/*压缩图片*/
+gulp.task('images', () => {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({ //使用cache只压缩改变的图片
+            optimizationLevel: 3, //压缩级别
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(browserSync.stream());
+});
+
+/*引用字体文件*/
+gulp.task('fonts', () => {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe(browserSync.stream());
 });
 
 /*css合并压缩，js合并压缩，html压缩*/
-gulp.task('html', ['styles', 'scripts'], () => {
+gulp.task('build', ['styles', 'scripts', 'images', 'fonts'], () => {
     let options = {
         removeComments: false, //清除HTML注释
         collapseWhitespace: true, //压缩HTML
@@ -58,7 +87,7 @@ gulp.task('html', ['styles', 'scripts'], () => {
 });
 
 /*本地建站和自动刷新*/
-gulp.task('serve', ['styles', 'scripts'], () => {
+gulp.task('serve', ['styles', 'scripts', 'html', 'images', 'fonts'], () => {
     browserSync({
         notify: false,
         port: 9000, //端口号
@@ -75,5 +104,7 @@ gulp.task('serve', ['styles', 'scripts'], () => {
 
     gulp.watch('app/styles/**/*.scss', ['styles']); //监测变化 执行styles任务
     gulp.watch('app/scripts/**/*.js', ['scripts']);
-    gulp.watch('app/*html', ['html']);
+    gulp.watch('app/fonts/**/*', ['fonts']);
+    gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('app/*.html', ['html']);
 });
